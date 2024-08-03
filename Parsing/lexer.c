a98f70cfd902fd038f63d66e604ff05129ca1cbe
BrahimJandri <bzaza066@gmail.com>
2024-08-03 15:31:05 +0100

    bismillah

--- Parsing/lexer.c
+++ Parsing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 07:46:41 by bjandri           #+#    #+#             */
-/*   Updated: 2024/08/03 15:00:33 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/08/03 15:30:50 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,65 +30,64 @@ static void	make_words(t_mini *shell, int start, int end)
 	ft_lstadd_back(&shell->head, ft_new_token(word));
 }
 
-static void	step_one(char *p, int *inside, char *quote, int i)
+static void step_one(char *p, int *inside, int i)
 {
-	if (*quote == 0)
-	{
-		*quote = p[i];
-		*inside = 1;
-	}
-	else if (*quote == p[i])
-	{
-		*quote = 0;
-		*inside = 0;
-	}
+    static char quote;
+
+    if (quote == 0)
+    {
+        quote = p[i];
+        *inside = 1;
+    }
+    else if (quote == p[i])
+    {
+        quote = 0;
+        *inside = 0;
+    }
 }
 
-static void	handle_character(t_mini *shell, int *i, t_split_params *params)
+static void handle_character(t_mini *shell, int *i, t_split_params *params)
 {
-	char	quote;
-
-	quote = 0;
-	if (shell->rl[*i] == '"' || shell->rl[*i] == '\'')
-		step_one(shell->rl, &params->inside, &quote, (*i)++);
-	else if (!(params->inside) && (is_whitespace(shell->rl[*i])
-			|| is_redirec(shell->rl[*i])))
-	{
-		params->end = *i;
-		if (params->end > params->start)
-			make_words(shell, params->start, params->end);
-		if (shell->rl[*i] == '|')
-			make_words(shell, *i, *i + 1);
-		if (shell->rl[*i] == '>' || shell->rl[*i] == '<')
-		{
-			params->end = count_redirec(shell->rl, *i);
-			make_words(shell, *i, params->end);
-			*i = params->end - 1;
-		}
-		while (is_whitespace(shell->rl[++(*i)]))
-			;
-		params->start = *i;
-	}
-	else
-		(*i)++;
+    if (shell->rl[*i] == '"' || shell->rl[*i] == '\'')
+        step_one(shell->rl, &params->inside, (*i)++);
+    else if (!(params->inside) && (is_whitespace(shell->rl[*i]) || is_redirec(shell->rl[*i])))
+    {
+        params->end = *i;
+        if (params->end > params->start)
+            make_words(shell, params->start, params->end);
+        if (shell->rl[*i] == '|')
+            make_words(shell, *i, *i + 1);
+        if (shell->rl[*i] == '>' || shell->rl[*i] == '<')
+        {
+            params->end = count_redirec(shell->rl, *i);
+            make_words(shell, *i, params->end);
+            *i = params->end - 1;
+        }
+        while (is_whitespace(shell->rl[++(*i)]))
+            ;
+        params->start = *i;
+    }
+    else
+        (*i)++;
 }
 
-static void	split_args(t_mini *shell)
+static void split_args(t_mini *shell)
 {
-	int				i;
-	t_split_params	params;
+    int i;
+    t_split_params params;
 
-	params.start = 0;
-	params.end = 0;
-	params.inside = 0;
-	i = 0;
-	while (shell->rl[i])
-	{
-		handle_character(shell, &i, &params);
-	}
-	if (i > params.start)
-		make_words(shell, params.start, i);
+    params.start = 0;
+    params.end = 0;
+    params.inside = 0;
+    i = 0;
+    while (shell->rl[i])
+    {
+        handle_character(shell, &i, &params);
+    }
+    if (i > params.start)
+        make_words(shell, params.start, i);
 }
+
 
 int parse_pipe(char *str)
 {
