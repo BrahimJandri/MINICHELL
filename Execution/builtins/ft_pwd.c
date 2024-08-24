b2c5eb8e7e912ca3409cf53cb0e8ba75b8134621
bjandri <bzaza066@gmail.com>
2024-08-24 12:15:56 +0100

    finish parsing

--- Execution/builtins/ft_pwd.c
+++ Execution/builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 10:47:25 by bjandri           #+#    #+#             */
-/*   Updated: 2024/08/24 10:04:10 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/08/24 12:00:24 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ int	pwd_builtin(t_env **env)
 
 	buf = getcwd(NULL, 0);
 	if (!buf)
-	{
 		ft_putendl_fd((*env)->pwd, 1);
-	}
 	ft_putendl_fd(buf, 1);
 	free(buf);
 	return (0);
