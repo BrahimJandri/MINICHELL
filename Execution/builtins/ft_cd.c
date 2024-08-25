7aad99c624aae372e15915c7df9c3c4181655e0d
BrahimJandri <bzaza066@gmail.com>
2024-08-25 08:50:26 +0100

    finish parsing and builtins

--- Execution/builtins/ft_cd.c
+++ Execution/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 10:52:11 by bjandri           #+#    #+#             */
-/*   Updated: 2024/08/24 12:12:24 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/08/24 14:22:53 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,29 +25,6 @@ int	is_valid_identifier(const char *str)
 	return (1);
 }
 
-// int	cd_builtin(char **args, t_env **env)
-// {
-// 	char	*path;
-// 	char	*oldpwd;
-
-// 	if (args[1] != NULL && args[2] != NULL)
-// 		return (ft_putendl_fd("minishell: cd: too many arguments", 2), 1);
-// 	oldpwd = getenv_value(*env, "PWD");
-// 	if (!args[1])
-// 		path = getenv_value(*env, "HOME");
-// 	else if (!ft_strncmp(args[1], "-", 1))
-// 		path = getenv_value(*env, "OLDPWD");
-// 	else
-// 		path = args[1];
-// 	if (chdir(path) == -1 || !path)
-// 		return (ft_putendl_fd("cd: no such file or directory", 2), 1);
-// 	else
-//     {
-// 		(*env)->pwd = getcwd(NULL, 0);
-//     	update_env(env, oldpwd, path);
-//     }
-// 	return (0);
-// }
 
 int cd_builtin(char **args, t_env **env)
 {
@@ -59,7 +36,11 @@ int cd_builtin(char **args, t_env **env)
         return (ft_putendl_fd("minishell: cd: too many arguments", 2), 1);
     oldpwd = getenv_value(*env, "PWD");
     if (!args[1])
+	{
         path = getenv_value(*env, "HOME");
+		if(!path)
+			return (ft_putendl_fd("cd: HOME not set", 2), 1);
+	}
     else
         path = args[1];
     if (chdir(path) == -1 || !path)
