802dd3ac6235cdae696a037a04ee0b2be446d02d
bjandri <bzaza066@gmail.com>
2024-08-28 18:24:23 +0100

    finish parsing

--- Execution/builtins/ft_cd.c
+++ Execution/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 10:52:11 by bjandri           #+#    #+#             */
-/*   Updated: 2024/08/28 17:44:46 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/08/28 18:23:34 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ int cd_builtin(char **args, t_env **env)
 {
     char *path;
     char *oldpwd;
-    char *new_pwd;
+
 
     if (prepare_cd(args, env, &path, &oldpwd) != 0)
         return 1;
--- Execution/ft_execution.c
+++ Execution/ft_execution.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 12:53:33 by rachid            #+#    #+#             */
-/*   Updated: 2024/08/28 17:46:03 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/08/28 18:23:28 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,7 +141,7 @@ void	execute_builtin(t_parser *args, t_mini *shell)
 	else if (ft_strncmp(args->cmd[0], "pwd", 3) == 0)
 		g_exit_status = pwd_builtin(&shell->env);
 	else if (ft_strncmp(args->cmd[0], "cd", 2) == 0)
-		g_exit_status = cd_builtin(args->cmd, shell->env);
+		g_exit_status = cd_builtin(args->cmd, &shell->env);
 	else if (ft_strncmp(args->cmd[0], "export", 6) == 0)
 		g_exit_status = export_builtin(args->cmd, shell);
 	else if (ft_strncmp(args->cmd[0], "unset", 5) == 0)
