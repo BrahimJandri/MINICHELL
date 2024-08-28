444a2be113b98019ae8e45494a0beee1bee81114
bjandri <bzaza066@gmail.com>
2024-08-28 09:21:39 +0100

    finish parsing

--- Execution/builtins/ft_cd.c
+++ Execution/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/28 10:52:11 by bjandri           #+#    #+#             */
-/*   Updated: 2024/08/27 20:49:40 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/08/28 09:16:09 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,44 +25,77 @@ int	is_valid_identifier(const char *str)
 	return (1);
 }
 
+// int cd_builtin(char **args, t_env **env)
+// {
+//     char *path;
+//     char *oldpwd;
+
+//     if (args[1] != NULL && args[2] != NULL)
+//         return (ft_putendl_fd("minishell: cd: too many arguments", 2), 1);
+//     oldpwd = getenv_value(*env, "PWD");
+//     if (!oldpwd)
+//     {
+//         oldpwd = getcwd(NULL, 0);
+//         if (!oldpwd)
+//             return (ft_putendl_fd("minishell: cd: failed to get current directory", 2), 1);
+//         update_env(env, "PWD", oldpwd);
+//         free(oldpwd);
+//     }
+//     if (!args[1])
+//     {
+//         path = getenv_value(*env, "HOME");
+//         if (!path)
+//             return (ft_putendl_fd("cd: HOME not set", 2), 1);
+//     }
+//     else
+//         path = args[1];
+//     if (chdir(path) == -1 || !path)
+//         return (perror("Minishell"), 1);
+//     free((*env)->pwd);
+//     (*env)->pwd = getcwd(NULL, 0);  // Allocate new PWD
+//     update_env(env, "OLDPWD", oldpwd);
+//     update_env(env, "PWD", (*env)->pwd);
+//     return (0);
+// }
+
+
+int prepare_cd(char **args, t_env **env, char **path, char **oldpwd)
+{
+    if (args[1] && args[2] )
+        return (ft_putendl_fd("minishell: cd: too many arguments", 2), 1);
+    *oldpwd = getenv_value(*env, "PWD");
+    if (!(*oldpwd))
+    {
+        *oldpwd = getcwd(NULL, 0);
+        if (!(*oldpwd))
+            return (perror("Minishell"), 1);
+        update_env(env, "PWD", *oldpwd);
+        free(*oldpwd);
+    }
+
+    if (!args[1])
+    {
+        *path = getenv_value(*env, "HOME");
+        if (!(*path))
+            return (ft_putendl_fd("cd: HOME not set", 2), 1);
+    }
+    else
+        *path = args[1];
+    return 0;
+}
+
 int cd_builtin(char **args, t_env **env)
 {
     char *path;
     char *oldpwd;
 
-    if (args[1] != NULL && args[2] != NULL)
-        return (ft_putendl_fd("minishell: cd: too many arguments", 2), 1);
-
-    oldpwd = getenv_value(*env, "PWD");
-    if (!oldpwd)
-    {
-        oldpwd = getcwd(NULL, 0);
-        if (!oldpwd)
-            return (ft_putendl_fd("minishell: cd: failed to get current directory", 2), 1);
-        update_env(env, "PWD", oldpwd);
-        free(oldpwd);  // Free the memory allocated by getcwd
-    }
-
-    if (!args[1])
-    {
-        path = getenv_value(*env, "HOME");
-        if (!path)
-            return (ft_putendl_fd("cd: HOME not set", 2), 1);
-    }
-    else
-    {
-        path = args[1];
-    }
-
+    if (prepare_cd(args, env, &path, &oldpwd) != 0)
+        return 1;
     if (chdir(path) == -1 || !path)
         return (perror("Minishell"), 1);
-
     free((*env)->pwd);
     (*env)->pwd = getcwd(NULL, 0);  // Allocate new PWD
-
     update_env(env, "OLDPWD", oldpwd);
     update_env(env, "PWD", (*env)->pwd);
-    
-    return (0);
+    return 0;
 }
-
