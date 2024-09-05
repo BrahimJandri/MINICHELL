833daffb33b75ebc799d83f43c6f4f5e93dd0e90
BrahimJandri <bzaza066@gmail.com>
2024-09-05 16:36:06 +0100

    yarbi aykml lkhirad

--- Parsing/ft_expand_herdoc.c
+++ Parsing/ft_expand_herdoc.c
@@ -6,11 +6,42 @@
 /*   By: bjandri <bjandri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 16:39:18 by bjandri           #+#    #+#             */
-/*   Updated: 2024/09/05 14:57:00 by bjandri          ###   ########.fr       */
+/*   Updated: 2024/09/05 16:33:17 by bjandri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+static char *extract_name_hrd(char *val, int *index, t_mini *shell)
+{
+    int i = *index;
+    char *str = ft_strdup("");
+
+    while (val[++i] && (ft_isalnum(val[i]) || val[i] == '_'))
+        str = ft_append_char(str, val[i]);
+    *index = i - 1;
+    char *ptr = get_value_env(str, shell);
+    free(str);
+    return ptr;
+}
+
+static void get_value_hrd(char *val, int *i, char **str, t_mini *shell)
+{
+    char *tmp = NULL;
+    char *new_str;
+
+    if (val[*i + 1] == '?')
+    {
+        (*i)++;
+        tmp = ft_itoa(g_exit_status);
+    }
+    else
+        tmp = extract_name_hrd(val, i, shell);
+    new_str = ft_strjoin(*str, tmp);
+    free(*str);
+    free(tmp);
+    *str = new_str;
+}
 
 static void	process_dollar_sign_herdoc(char *val, int *i, char **str,
 		t_mini *shell)
@@ -19,7 +50,7 @@ static void	process_dollar_sign_herdoc(char *val, int *i, char **str,
 	if (val[*i + 1] && is_val_char(val[*i + 1]))
 	{
 		if (val[*i + 1] != '$' && !ft_isdigit(val[*i + 1]))
-			get_value(val, i, str, shell);
+			get_value_hrd(val, i, str, shell);
 		else
 			(*i)++;
 	}
